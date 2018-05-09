15702d29c84e69bc9421f4c551d7474f145a86d3
mgacy <matt@mgacy.com>
2018-05-09 13:36:01 -0700

    Hold ‘d’ for LOWER

--- keyboards/xd75/keymaps/mgacy/keymap.c
+++ keyboards/xd75/keymaps/mgacy/keymap.c
@@ -32,7 +32,9 @@
 // Custom
 #define HPR_ESC   ALL_T(KC_ESC)       // Tap for Escape, hold for Hyper (Cmnd+Ctrl+Opt+Shift)
 #define MEH_GRV   MEH_T(KC_GRV)       // Tap for Backtick, hold for Meh (Ctrl+Opt+Shift)
-#define NAV_SCL   LT(_NV, KC_SCLN)    // Tap for semicolon, hold for NAV layer
+// Layers
+#define NAV_SCL   LT(_NV, KC_SCLN)    // Tap for semicolon, hold for NAVIGATION layer
+#define LW_KC_D   LT(_LW, KC_D)       // Tap for d, hold for LOWERED (Symbol) layer
 // Navigation
 #define NXT_APP   LGUI(KC_TAB)        // MAC: switch to next application
 #define PRV_APP   LGUI(S(KC_TAB))     // MAC: switch to prev application
@@ -62,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | TAB    | Q      | W      | E      | R      | T      | -      | PG UP  | =      | Y      | U      | I      | O      | P      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * | ESC    | A      | S      | D      | F      | G      | [      | PG DN  | ]      | H      | J      | K      | L      | ;      | RETURN |
- * |-/HYPER-+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-/NAV---+--------|
+ * |-/HYPER-+--------+--------+-/LOWER-+--------+--------+--------+--------+--------+--------+--------+--------+--------+-/NAV---+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | LOWER  | UP     | RAISE  | N      | M      | ,      | .      | /      | RSHIFT |
  * |-/(-----+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-/)-----|
  * | FN     | LCTRL  | LALT   | LGUI   | SPACE  | BACKSP | LEFT   | DOWN   | RIGHT  | RETURN | SPACE  | RGUI   | ENTER  | ????   | MOUSE  |
@@ -72,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QW] = { /* QWERTY */
   { MEH_GRV, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_HOME, KC_BSLS, KC_END , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC},
   { KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_MINS, KC_PGUP, KC_EQL , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_QUOT},
-  { HPR_ESC, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_LBRC, KC_PGDN, KC_RBRC, KC_H   , KC_J   , KC_K   , KC_L   , NAV_SCL, KC_ENT },
+  { HPR_ESC, KC_A   , KC_S   , LW_KC_D, KC_F   , KC_G   , KC_LBRC, KC_PGDN, KC_RBRC, KC_H   , KC_J   , KC_K   , KC_L   , NAV_SCL, KC_ENT },
   { KC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , MO(_LW), KC_UP  , MO(_RS), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSPC},  
   { MO(_FN), KC_LCTL, KC_LALT, KC_LCMD, KC_SPC , KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT , KC_SPC , KC_RCMD, KC_PENT, KC_PDOT, MO(_MS)},
  },
