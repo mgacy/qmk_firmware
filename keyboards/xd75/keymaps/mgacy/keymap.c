5d0c1eba92f101401277fa1f895abad832672c50
mgacy <matt@mgacy.com>
2018-05-19 12:55:32 -0700

    Hold spacebars for modifiers
    Remove modifiers from Navigation layer

--- keyboards/xd75/keymaps/mgacy/keymap.c
+++ keyboards/xd75/keymaps/mgacy/keymap.c
@@ -34,6 +34,8 @@
 // Custom Aliases
 #define HPR_ESC   ALL_T(KC_ESC)       // Tap for Escape, hold for Hyper (Cmnd+Ctrl+Opt+Shift)
 #define MEH_GRV   MEH_T(KC_GRV)       // Tap for Backtick, hold for Meh (Ctrl+Opt+Shift)
+#define CMD_SPC   LGUI_T(KC_SPC)      // Tap for Spacebar, hold for Command
+#define OPT_SPC   LALT_T(KC_SPC)      // Tap for Spacebar, hold for Option
 // Layers
 #define NAV_SCL   LT(_NV, KC_SCLN)    // Tap for semicolon, hold for NAVIGATION layer
 #define SY_KC_D   LT(_SY, KC_D)       // Tap for d, hold for SYMBOL layer
@@ -76,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LSHIFT | Z      | X      | C      | V      | B      | LOWER  | UP     | RAISE  | N      | M      | ,      | .      | /      | RSHIFT |
  * |-/(-----+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-/)-----|
  * | FN     | LCTRL  | LALT   | LGUI   | BACKSP | BACKSP | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | RGUI   | ENTER  | ????   | MOUSE  |
- * '--------------------------------------------------------------------------------------------------------------------------------------'
+ * '----------------------------------------------------------------------------------/CMND----/OPT---------------------------------------'
  */
 	
  [_QW] = { /* QWERTY */
@@ -84,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_MINS, KC_PGUP, KC_EQL , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_QUOT},
   { HPR_ESC, KC_A   , KC_S   , SY_KC_D, TX_KC_F, KC_G   , KC_LBRC, KC_PGDN, KC_RBRC, KC_H   , KC_J   , KC_K   , KC_L   , NAV_SCL, KC_ENT },
   { KC_LSPO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , MO(_LW), KC_UP  , MO(_RS), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSPC},  
-  { MO(_FN), KC_LCTL, KC_LALT, KC_LCMD, KC_BSPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_SPC , KC_SPC , KC_RCMD, KC_PENT, KC_PDOT, MO(_MS)},
+  { MO(_FN), KC_LCTL, KC_LALT, KC_LCMD, KC_BSPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, CMD_SPC, OPT_SPC, KC_RCMD, KC_PENT, KC_PDOT, MO(_MS)},
  },
 
 /* LOWERED
@@ -163,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * |        | SPACE- | APP-   | WIN-   | TAB-   | TAB2-  |        |        |        | END    | PG DN  |        |        |        |        |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * |        |        |        |        |        |        |        |        |        | RGUI   | LALT   |        |        |        |        |
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -172,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { _______, _______, PRV_APP, PRV_WIN, PRV_TAB, _______, _______, _______, _______, KC_HOME, KC_PGUP, _______, _______, _______, _______},
   { _______, _______, NXT_APP, NXT_WIN, NXT_TAB, NXT_ATB, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______},
   { _______, _______, PRV_APP, PRV_WIN, PRV_TAB, PRV_ATB, _______, _______, _______, KC_END , KC_PGDN, _______, _______, _______, _______},
-  { _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RCMD, KC_LALT, _______, _______, _______, _______},
+  { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
  },
 
 /* TEXT MANIPULATION
